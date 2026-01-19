d16094ba11087a462830609439782ddeb8996859
Monther5 <monder_4207@limu.edu.ly>
2026-01-19 13:55:47 +0200

    Create Address.php

---
+++ app/Models/Address.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Address extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'name',
+        'full_address',
+        'description',
+        'latitude',
+        'longitude',
+        'phone',
+        'is_default',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'latitude' => 'decimal:8',
+        'longitude' => 'decimal:8',
+        'is_default' => 'boolean',
+    ];
+
+    /**
+     * Get the user that owns the address.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
